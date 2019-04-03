fdade99c0f33a7a03b306c40a84c7d0384982861
Philipp Wagner <wagner@getrequest.de>
2019-04-03 08:25:23 +0200

    start using typescript (cubbles/cubbles#27)

--- src/launcher/launcher.ts
+++ src/launcher/launcher.ts
@@ -1,6 +1,19 @@
 // import all resources here
 import css from './c-util.sss';
 
-(() => {
-    console.log('Hello world! I am util /* @echo elementName */');
-})();
+class Launcher {
+    constructor () {
+        this.drawLauncherIcon();
+    }
+
+    private drawLauncherIcon () {
+        // append launcher to dom using absolute positioning
+        const launcherIcon = document.createElement('div');
+        launcherIcon.innerHTML = '<p>Show launcher</p>';
+        launcherIcon.addEventListener('click', () => { alert('hello!'); });
+
+        document.body.appendChild(launcherIcon);
+    }
+}
+
+const launcher = new Launcher();
