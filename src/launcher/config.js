513eb9eb053640d506ffcb0f0adb0b7b2091b5d6
Philipp Wagner <wagner@getrequest.de>
2019-04-10 23:52:03 +0200

    add config based menu (cubbles/cubbles#27)

---
+++ src/launcher/config.js
@@ -0,0 +1,35 @@
+class Config {
+  getConfig () {
+    return {
+      launcherIcon: {
+        collapsed: 'apps',
+        expanded: 'keyboard_arrow_right'
+      },
+      apps: [
+        {
+          icon: 'playlist_add'
+        },
+        {
+          icon: 'speaker_phone'
+        },
+        {
+          icon: 'location_on'
+        },
+        {
+          icon: 'forum'
+        },
+        {
+          icon: 'message'
+        },
+        {
+          icon: 'ring_volume'
+        },
+        {
+          icon: 'ev_station'
+        }
+      ]
+    };
+  }
+}
+
+export default Config;
--- src/launcher/launcher.js
+++ src/launcher/launcher.js
@@ -1,20 +1,67 @@
 // import all resources here
-import css from './launcher.sss';
+import css from './launcher.sss'; // eslint-disable-line no-unused-vars
+import Config from './config';
 
 class Launcher {
-    constructor () {
-        this.drawLauncherIcon();
-    }
+  constructor () {
+    this.config = (new Config()).getConfig();
+    this.drawLauncherIcon();
+    this.drawLauncherMenu();
+  }
 
-    drawLauncherIcon () {
-        // append launcher to dom using absolute positioning
-        const launcherIcon = document.createElement('div');
-        launcherIcon.classList.add('/* @echo webpackageName */_cubx-launcher-root');
-        launcherIcon.innerHTML = '<i class="/* @echo webpackageName */_material-icons">chevron_left</i>';
-        launcherIcon.addEventListener('click', () => { alert('hello!'); });
+  drawLauncherIcon () {
+      // append launcher to dom using absolute positioning
+    const launcherIcon = document.createElement('div');
+    launcherIcon.classList.add('/* @echo webpackageName */_cubx-launcher-root');
+    launcherIcon.innerHTML = `<i class="/* @echo webpackageName */_material-icons">${this.config.launcherIcon.collapsed}</i>`;
 
-        document.body.appendChild(launcherIcon);
+    launcherIcon.addEventListener('click', () => { this.toggleMenu(); });
+
+    document.body.appendChild(launcherIcon);
+    this.launcherIcon = launcherIcon;
+  }
+
+  drawLauncherMenu () {
+    const launcherMenu = document.createElement('div');
+    launcherMenu.style.visibility = 'hidden';
+    launcherMenu.classList.add('/* @echo webpackageName */_cubx-launcher-menu');
+
+    let markup = '<ul>';
+    this.config.apps.forEach((config, index) => {
+      markup += `<li class="/* @echo webpackageName */_cubx-launcher-app-icon" data-index="${index}"><i class="/* @echo webpackageName */_material-icons">${config.icon}</i></li>`;
+    });
+    markup += '</ul>';
+
+    launcherMenu.innerHTML = markup;
+    launcherMenu.querySelectorAll('li').forEach(node => {
+      ((node) => {
+        node.addEventListener('click', () => {
+          const index = parseInt(node.getAttribute('data-index'));
+          this.drawModalPopup(this.config.apps[index]);
+        });
+      })(node);
+    });
+    document.body.appendChild(launcherMenu);
+    this.launcherMenu = launcherMenu;
+  }
+
+  drawModalPopup (config) {
+    console.log(config);
+  }
+
+  toggleMenu () {
+    const elem = this.launcherIcon.querySelector('i');
+    switch (elem.innerText) {
+      case this.config.launcherIcon.collapsed:
+        elem.innerText = this.config.launcherIcon.expanded;
+        this.launcherMenu.style.visibility = 'visible';
+        break;
+      case this.config.launcherIcon.expanded:
+        elem.innerText = this.config.launcherIcon.collapsed;
+        this.launcherMenu.style.visibility = 'hidden';
+        break;
     }
+  }
 }
 
-const launcher = new Launcher();
+const launcher = new Launcher(); // eslint-disable-line no-unused-vars
